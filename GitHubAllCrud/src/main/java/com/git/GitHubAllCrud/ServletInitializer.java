356d1ba981b141ce68691fe6369b6ce383f74215
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-09-09 18:52:31 +0530

    All Done

---
+++ GitHubAllCrud/src/main/java/com/git/GitHubAllCrud/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.git.GitHubAllCrud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GitHubAllCrudApplication.class);
+	}
+
+}
---
+++ GitHubAllCrud/src/test/java/com/git/GitHubAllCrud/GitHubAllCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.GitHubAllCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubAllCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
